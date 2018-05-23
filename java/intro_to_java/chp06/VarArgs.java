91919ca1c81b89318f5639e009ce6d12db469584
Nick Calibey <nick.calibey@gmail.com>
2018-05-23 08:30:31 -0500

    added more chp 6 exercises

---
+++ java/intro_to_java/chp06/VarArgs.java
@@ -0,0 +1,19 @@
+// Demonstrate variable-length arguments
+class VarArgs {
+  // vaTest() uses a varaarg
+  static void vaTest(int ... v) {
+    System.out.println("Number of args: " + v.length);
+    System.out.println("Contents: ");
+
+    for (int i = 0; i < v.length; i++)
+      System.out.println("  arg " + i + ": " + v[i]);
+    System.out.println();
+  }
+
+  public static void main(String[] args) {
+    // Notice how vaTest() can be called with a variable number of arguments
+    vaTest(10);
+    vaTest(1, 2, 3);
+    vaTest();
+  }
+}
