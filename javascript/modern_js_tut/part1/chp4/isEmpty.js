695ce75a62c9520531b60566860c42865016adf3
Nick Calibey <nick.calibey@gmail.com>
2018-05-28 19:40:11 -0500

    finished Objects tutorial

---
+++ javascript/modern_js_tut/part1/chp4/isEmpty.js
@@ -0,0 +1,9 @@
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+let schedule = {};
+
+console.log(isEmpty(schedule));
+schedule["8:30"] = "get up";
+console.log(isEmpty(schedule));
