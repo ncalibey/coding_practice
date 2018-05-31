6f9aa7cebd874d0466a17569d5f72390b2103bad
Nick Calibey <nick.calibey@gmail.com>
2018-05-30 23:26:52 -0500

    finished chp 7 examples

---
+++ java/intro_to_java/chp07/AbsShape.java
@@ -0,0 +1,135 @@
+// create an abstract class
+abstract class TwoDShape {
+  private double width;
+  private double height;
+  private String name;
+
+  // a default constructor
+  TwoDShape() {
+    width = height = 0.0;
+    name = "none";
+  }
+
+  // Parameterized constructor
+  TwoDShape(double w, double h, String n) {
+    width = w;
+    height = h;
+    name = n;
+  }
+
+  // Construct object with equal width and height
+  TwoDShape(double x, String n) {
+    width = height = x;
+    name = n;
+  }
+
+  // Construct an object from an object
+  TwoDShape(TwoDShape ob) {
+    width = ob.width;
+    height = ob.height;
+    name = ob.name;
+  }
+
+  // Accessor methods for width and height
+  double getWidth() { return width; }
+  double getHeight() { return height; }
+  void setWidth(double w) { width = w; }
+  void setHeight(double h) { height = h; }
+
+  String getName() { return name; }
+
+  void showDim() {
+    System.out.println("Width and height are " + width + " " + height);
+  }
+
+  // Now, area() is abstract
+  abstract double area();
+}
+
+// A subclass of TwoDShape for triangles
+class Triangle extends TwoDShape {
+  String style;
+
+  // A default constructor
+  Triangle() {
+    super();
+    style = "none";
+  }
+
+  // Constructor for Triangle
+  Triangle(String s, double w, double h) {
+    super(w, h, "triangle");
+    style = s;
+  }
+
+  // One argument constructor
+  Triangle(double x) {
+    super(x, "triangle");
+    style = "filled";
+  }
+
+  // Construct an object from an object
+  Triangle(Triangle ob) {
+    super(ob); // pass object to TwoDShape constructor
+    style = ob.style;
+  }
+
+  // Override area() for Triangle
+  double area() {
+    return getWidth() * getHeight() / 2;
+  }
+
+  void showStyle() {
+    System.out.println("Triangle is " + style);
+  }
+}
+
+// A subclass of TwoDShape for rectangles
+class Rectangle extends TwoDShape {
+  // A default constructor
+  Rectangle() {
+    super();
+  }
+
+  // Constructor for Rectangle
+  Rectangle(double w, double h) {
+    super(w, h, "rectangle");
+  }
+
+  // Construct a square
+  Rectangle(double x) {
+    super(x, "rectangle");
+  }
+
+  // Construct an object from an object
+  Rectangle(Rectangle ob) {
+    super(ob);
+  }
+
+  boolean isSquare() {
+    if (getWidth() == getHeight()) return true;
+    return false;
+  }
+
+  // Override area() for Rectangle
+  double area() {
+    return getWidth() * getHeight();
+  }
+}
+
+class AbsShape {
+  public static void main(String[] args) {
+    TwoDShape shapes[] = new TwoDShape[4];
+
+    shapes[0] = new Triangle("outlined", 8.0, 12.0);
+    shapes[1] = new Rectangle(10);
+    shapes[2] = new Rectangle(10, 4);
+    shapes[3] = new Triangle(7.0);
+
+    for (int i = 0; i < shapes.length; i++) {
+      System.out.println("object is " + shapes[i].getName());
+      System.out.println("Area is " + shapes[i].area());
+      System.out.println();
+    }
+  }
+}
